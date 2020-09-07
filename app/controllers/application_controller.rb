622f45e5858d9cb802ca53414caefda2433aa253
sando0710 <54323405+sando0710@users.noreply.github.com>
2020-09-08 08:14:07 +0900

    コンフリクションの解消

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,16 +1,3 @@
 class ApplicationController < ActionController::Base
-<<<<<<< HEAD
-=======
-  before_action :basic_auth
-
-
-  private
-
-  def basic_auth
-    authenticate_or_request_with_http_basic do |username, password|
-      username == 'admin' && password == '2222'
-      username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
-    end
-  end
->>>>>>> parent of cc90d89... Revert "BASIC機能実装"
+  
 end
